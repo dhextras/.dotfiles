8a0f761c29546c40183c6c5818f05193e2b589d4
dhextras <mohammedsafki27@gmail.com>
2024-09-20 15:35:25 +0530

    feat: few more keybindings

--- nvim/.config/nvim/lua/dhextras/plugins/autoformat.lua
+++ nvim/.config/nvim/lua/dhextras/plugins/autoformat.lua
@@ -39,6 +39,7 @@ return {
         javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         json = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
