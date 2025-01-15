1d4e619360ac496eb785ace593bb083ffde2069c
dhextras <mohammedsafki27@gmail.com>
2025-01-15 08:43:27 +0530

    chore: increase timeout for formatting

--- nvim/.config/nvim/lua/dhextras/plugins/autoformat.lua
+++ nvim/.config/nvim/lua/dhextras/plugins/autoformat.lua
@@ -21,7 +21,7 @@ return {
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
-          timeout_ms = 500,
+          timeout_ms = 5000,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
