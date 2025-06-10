97d461d62e466e8744c1ca0ea2a507c1f6e1aa0d
dhextras <mohammedsafki27@gmail.com>
2025-06-10 20:33:21 +0800

    chore: fix idk

--- cscripts/.local/src/cscripts/tagsmode.sh
+++ cscripts/.local/src/cscripts/tagsmode.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
 FLAG="$HOME/.cache/polybar_show_all_tags"
 [[ ! -f "$FLAG" ]] && echo "0" > "$FLAG"
-
 mode=$(cat "$FLAG")
-
 if [ "$mode" = "1" ]; then
-  sed -i 's/modules-center =.*$/modules-center = tagsbar_all/' ~/.dotfiles/polybar/.config/polybar/config.ini
+  sed -i 's/modules-center =.$/modules-center = tagsbar_all/' ~/.dotfiles/polybar/.config/polybar/config.ini
 else
-  sed -i 's/modules-center =.*$/modules-center = tagsbar_active/' ~/.dotfiles/polybar/.config/polybar/config.ini
+  sed -i 's/modules-center =.$/modules-center = tagsbar_active/' ~/.dotfiles/polybar/.config/polybar/config.ini
 fi
-
 polybar-msg cmd restart
-
